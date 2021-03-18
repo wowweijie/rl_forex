4c8f2ee9f8188ed47b3d4f1d66d1285df41f9504
Wong Wei Jie <weighzhie@gmail.com>
2021-03-18 23:34:12 +0800

    push main notebook and environment.yml

--- finrl/trade/backtest.py
+++ finrl/trade/backtest.py
@@ -74,7 +74,7 @@ def backtest_strat(df):
     strategy_ret = df.copy()
     strategy_ret["date"] = pd.to_datetime(strategy_ret["date"])
     strategy_ret.set_index("date", drop=False, inplace=True)
-    strategy_ret.index = strategy_ret.index.tz_localize("UTC")
+    #strategy_ret.index = strategy_ret.index.tz_localize("UTC")
     del strategy_ret["date"]
     ts = pd.Series(strategy_ret["daily_return"].values, index=strategy_ret.index)
     return ts
